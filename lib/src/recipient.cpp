1c1f1f6dc2b5eedd96a36b6f6bb85a95cb33f755
c5xheavy <amir-baturov@mail.ru>
2023-12-10 19:03:23 +0300

    add sender and recipient executables

---
+++ lib/src/recipient.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/asio.hpp>
+
+#include "datagram_deserializer.h"
+
+namespace net = boost::asio;
+using net::ip::udp;
+
+int main() {
+    std::uint16_t port = 3333;
+    std::size_t max_buffer_size = 1024;
+
+    try {
+        net::io_context io_context;
+        udp::socket socket{io_context, udp::endpoint{udp::v4(), port}};
+
+        while (true) {
+            std::vector<std::uint8_t> buffer(max_buffer_size);  
+
+            udp::endpoint remote_endpoint;
+            std::size_t size = socket.receive_from(net::buffer(buffer), remote_endpoint);
+            socket.send_to(net::buffer("Hello from recipient"), remote_endpoint);
+
+            buffer.resize(size);
+            buffer.shrink_to_fit();
+
+            Datagram datagram = DatagramDeserializer::DatagramFromBuffer(buffer);
+
+            std::cout << datagram.id << '\n';
+            std::cout << datagram.sender_login << '\n';
+            std::cout << datagram.recipient_login << '\n';
+            std::cout << datagram.message.sender_ip << '\n';
+            std::cout << datagram.message.time << '\n';
+            std::cout << datagram.message.text << '\n';
+        }
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
---
+++ lib/src/sender.cpp
@@ -0,0 +1,42 @@
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/asio.hpp>
+
+#include "datagram_serializer.h"
+
+namespace net = boost::asio;
+using net::ip::udp;
+
+int main() {
+    std::string ip = "127.0.0.1";
+    std::uint16_t port = 3333;
+
+    try {
+        Datagram datagram {
+            1,
+            "sender",
+            "recipient",
+            {
+                "127.0.0.1",
+                1700000000,
+                "hello"
+            }
+        };
+        std::vector<std::uint8_t> buffer{DatagramSerializer::DatagramToBuffer(datagram)};
+
+        net::io_context io_context;
+        udp::socket socket{io_context, udp::v4()};
+        udp::endpoint endpoint{net::ip::make_address(ip), port};
+        socket.send_to(net::buffer(buffer), endpoint);
+
+        std::array<char, 128> recv_buf;
+        udp::endpoint remote_endpoint;
+        std::size_t size = socket.receive_from(net::buffer(recv_buf), remote_endpoint);
+        std::cout.write(recv_buf.data(), size) << '\n';
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}

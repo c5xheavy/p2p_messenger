50eb056819aa91366b33834b62cd013bfcb99bdf
c5xheavy <amir-baturov@mail.ru>
2024-04-18 00:12:13 +0300

    Add sendButton click event handler in MainWindow class

--- lib/src/mainwindow.cpp
+++ lib/src/mainwindow.cpp
@@ -1,6 +1,17 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <iostream>
+#include <string>
+#include <thread>
+
+// need this to avoid conflict with Qt's emit keyword
+#ifdef emit
+#undef emit
+#include <syncstream>
+#define emit
+#endif
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +24,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_sendButton_clicked()
+{
+    std::osyncstream(std::cout) << '[' << std::hash<std::thread::id>{}(std::this_thread::get_id()) << "] " << "Send button is clicked" << std::endl;
+    std::string message = ui->messageEdit->text().toStdString();
+    if (message.empty()) {
+        std::osyncstream(std::cout) << '[' << std::hash<std::thread::id>{}(std::this_thread::get_id()) << "] " << "Message is empty" << std::endl;
+        return;
+    }
+    ui->messageEdit->clear();
+    std::osyncstream(std::cout) << '[' << std::hash<std::thread::id>{}(std::this_thread::get_id()) << "] " << "Sending message: " << message << std::endl;
+}

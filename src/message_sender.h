8f2d47acb4782e47999d765c44259fc7f94d4d6b
c5xheavy <amir-baturov@mail.ru>
2024-06-05 01:33:58 +0300

    change 2 sockets to 1 socket

--- src/message_sender.h
+++ src/message_sender.h
@@ -18,7 +18,7 @@ class MessageSender {
 public:
     using SendMessageHandler = std::function<void(Message&& message)>;
 
-    MessageSender(net::io_context& io_context, DhtIpResolver& dht_ip_resolver, const std::string& source_ip, uint16_t source_port,
+    MessageSender(udp::socket& socket, DhtIpResolver& dht_ip_resolver, const std::string& source_ip, uint16_t source_port,
                   const std::string& source_login, const dht::crypto::Identity& identity, SendMessageHandler handler);
     ~MessageSender();
 
@@ -29,13 +29,12 @@ private:
     std::pair<std::string, uint16_t> get_ip_and_port_from_address(const std::string& address);
 
 private:
-    net::io_context& io_context_;
+    udp::socket& socket_;
     DhtIpResolver& dht_ip_resolver_;
     std::string source_ip_;
     uint16_t source_port_;
     std::string source_login_;
     dht::crypto::Identity identity_;
-    udp::socket socket_;
     SendMessageHandler handler_;
 };
 
--- src/p2p_messenger_impl.cpp
+++ src/p2p_messenger_impl.cpp
@@ -25,12 +25,13 @@ P2PMessengerImpl::P2PMessengerImpl(const std::string& my_login, uint16_t dht_por
     , threads_{}
     , io_context_{static_cast<int>(num_threads_)}
     , work_guard_{net::make_work_guard(io_context_)}
-    , message_sender_{io_context_, dht_ip_resolver_, my_ip_, my_port_, my_login_, identity_, [this, send_message_handler = std::move(send_message_handler)](Message&& message) {
+    , socket_{io_context_, udp::endpoint{udp::v4(), my_port_}}
+    , message_sender_{socket_, dht_ip_resolver_, my_ip_, my_port_, my_login_, identity_, [this, send_message_handler = std::move(send_message_handler)](Message&& message) {
         std::osyncstream(std::cout) << '[' << std::hash<std::thread::id>{}(std::this_thread::get_id()) << "] " << "Message sent" << std::endl;
         chat_history_.append(dht::crypto::PublicKey{message.destination_public_key}.getId(), message.source_login, message.payload.text);
         send_message_handler(std::move(message));
     }}
-    , message_receiver_{io_context_, my_port_, identity_.first, [this, receive_message_handler = std::move(receive_message_handler), listen_login_handler](Message&& message) {
+    , message_receiver_{socket_, identity_.first, [this, receive_message_handler = std::move(receive_message_handler), listen_login_handler](Message&& message) {
         std::osyncstream(std::cout) << '[' << std::hash<std::thread::id>{}(std::this_thread::get_id()) << "] " << "Message received" << std::endl;
         chat_history_.append(dht::crypto::PublicKey{message.source_public_key}.getId(), message.source_login, message.payload.text);
         metadata_ip_resolver_.put(message.source_login, std::make_shared<dht::crypto::PublicKey>(message.source_public_key), message.source_ip, message.source_port);
